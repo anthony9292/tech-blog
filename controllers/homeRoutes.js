2a8ce7a4c166d236022cc07f815998cf71ce69e5
Anthony Ndegwa <anthon.n24@gmail.com>
2021-06-10 16:09:06 -0400

    first step layout

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,103 @@
+const router = require('express').Router();
+const { User, Product, Messages } = require('../models');
+const dotenv = require('dotenv').config()
+
+router.get('/', async (req, res) => {
+    try {
+        const productData = await Product.findAll({
+            include: {
+                model: User,
+                as: 'user'
+            },
+            order:[['updatedAt',  'DESC']]
+        });
+        const products = productData.map((product) => product.get({ plain: true }));
+        const name = products[0].user.name
+        res.render('homepage', { products, name, logged_in:req.session.logged_in });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    try {
+        if (req.session.logged_in) {
+            res.redirect('/');
+            return;
+          }
+        
+          res.render('login');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/messages', async (req, res) => {
+    try {
+        const messageData = await Messages.findAll({
+            where: {user_id: req.session.user_id}, 
+            order:[['updatedAt',  'DESC']]
+        });
+
+        const userData = await User.findOne({ where: {id: req.session.user_id } });
+        console.log("userData", userData.name);
+
+        const messages = messageData.map((messages) => messages.get({ plain: true }));
+        messages.map(item => {
+            try {
+                item.messages = cryptr.decrypt(item.messages);   
+                item.username = userData.name;                
+                return item;
+            } catch(ex) {
+                console.error(ex);
+            }
+        });
+        
+        res.render('messages', { messages, logged_in: req.session.logged_in });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/pantry', async (req, res) => {
+
+    try {
+        const pantryData = await Product.findAll({
+            where: {user_id: req.session.user_id},
+            order:[['updatedAt',  'DESC']]
+        });
+        
+        const pantry = pantryData.map((pantry) => pantry.get({ plain: true }));
+        console.log(pantry)
+        res.render('pantry', { pantry, logged_in: req.session.logged_in });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/register', async (req, res) => {
+    try{
+        res.render('register')
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+
+router.get('/create', async (req, res) => {
+    try{
+        res.render('create', {logged_in: req.session.logged_in})
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+router.get('/messageform', async (req, res) => {
+    try{
+        res.render('messageform', {logged_in: req.session.logged_in})
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+module.exports = router;
---
+++ seeds/seed.js
@@ -0,0 +1,27 @@
+const sequelize = require('../config/connection');
+const { User, Product, Messages } = require('../models');
+const userData = require('./seedUser.json');
+const postData = require('./seedPost.json');
+const messageData = require('./seedMessage.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+  
+   await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    });
+  
+    await Comment.bulkCreate(postData, {
+        individualHooks: true,
+        returning: true,
+      });
+    
+    await Messages.bulkCreate(messageData, {
+      individualHooks: true,
+      returning: true,
+    });
+    
+    process.exit(0);
+  };
+seedDatabase();
