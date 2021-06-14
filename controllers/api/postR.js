d5dda8d212fdcfe38a13c0a2b858f2c9cac92399
Anthony Ndegwa <anthon.n24@gmail.com>
2021-06-14 16:48:31 -0400

    api changed

---
+++ controllers/api/postR.js
@@ -0,0 +1,61 @@
+const router = require('express').Router;
+const { Post } = require ('../../models');
+
+router.post('/', async (req, res) => { 
+try { 
+    if(req.session.logged_in)  { 
+        const postData = await Post.create({ 
+            title: req.body.title, 
+            text: req.body.text, 
+            user_id: req.session.user_id
+        }) 
+        res.status(200).json(postData);
+}else { 
+    res.redirect('/login');
+} 
+}catch (err) { 
+    res.status(400).json(err);
+}
+
+});
+
+router.delete('/:id', async (req, res) => { 
+    try{ 
+        if(req.session.logged_in) { 
+            const postData = await Post.destroy({ 
+                where: { 
+                    id: req.params.id
+                }
+            })
+            res.status(200).json(postData); 
+    }else {
+        res.redirect('/login'); 
+    }
+
+    } catch (err) { 
+        res.status(400).json(err); 
+    }
+});
+
+router.put('/:id', async (req,res) => { 
+    try{
+        if(req.session.logged_in) { 
+            const postData = await Post.update({ 
+                title: req.body.title, 
+                text: req.body.text 
+            }, 
+            {   
+                where: { 
+                    id: req.params.id
+                },
+            })
+            res.status(200).json(postData); 
+        } else { 
+            res.redirect('login'); 
+        }
+    } catch (err) { 
+        res.status(400).json(err);
+    }
+})
+
+module.exports = router; 
