f536245a6b6fc2d558ec4777eb16732dde0fe917
Anthony Ndegwa <anthon.n24@gmail.com>
2021-06-14 17:20:02 -0400

    api done

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const userR = require('./api/userR');
 const postR = require('./api/postR');
-const commentR = 
+const commentR = require('./api/commentR');
 
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+router.use('/users', userR);
+router.use('/posts', postR);
+router.use('/comments', commentR ); 
+
 
 module.exports = router;
