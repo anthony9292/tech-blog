a8b42d3ebc567f7315801d9b5477b3c27c504486
Anthony Ndegwa <anthon.n24@gmail.com>
2021-06-16 18:16:36 -0400

    all edits so far

--- controllers/api/post-r.js
+++ controllers/api/post-r.js
@@ -1,9 +1,10 @@
 const router = require('express').Router(); 
 const { Post, User, Comment } = require('../../models');
 const sequelize = require("../../config/connection"); 
+const { route } = require('.');
 
 ///creates new post 
-router.post('/',(req, res) => { 
+router.post('/', (req, res) => { 
     Post.create({ 
         title: req.body.title,  
         body: req.body.body, 
@@ -21,4 +22,31 @@ router.put('/:id',  req, res => {
             id: req.params.id,
         }
     })
-})
+    then.((posts) => { 
+
+        if(!req.params.id) { 
+            res.status(404).json({ message: "Post matching that id not found!!"}); 
+            return; 
+        }
+        res.json(posts)
+    }) 
+    .catch ((err) => res.status(400).json(err))
+}); 
+
+router.delete('/:id', (req, res) => {
+    Post.destroy({ 
+        where: { 
+            id: req.params.id, 
+        }, 
+    })
+    .then((posts) => { 
+        if(!req.params.id) { 
+            res.status(404).json({messge:"Post matching that id not found!!"}); 
+            return;
+        }
+        res.json(posts)
+    })
+    .catch ((err) => res.status(500).json(err))
+}); 
+
+module.exports = router;
