362252246a4bed19a9f2cb0dd0ce2a41c6ce45b9
Anthony Ndegwa <anthon.n24@gmail.com>
2021-06-30 17:41:53 -0400

    reowrking controllers start at homeR

--- controllers/homeR.js
+++ controllers/homeR.js
@@ -1,19 +1,115 @@
 const router = require('express').Router();
-const { Post, User, Comment } = require('../models'); 
+const { User, Post, Comment} = require('../models');
 
 
+router.get('/', async (req, res) => {
+    try {
+          if ( req.session.logged_in) 
+           const postData = await Post.findAll({
+               include: { 
+                   model: User,
+                   as: 'user'
+               }, 
+               order: [['updatedAt', 'DESC']]
+           }); 
+           const posts = postData.map((post) => post.get({ plain: true })); 
+           res.render('homepage', { posts, logged_in: req.session.logged_in }); 
+        } 
+        else { 
+            const postData = await Post.findAll({ 
+                include: { 
+                    model: 'User',
+                    as: 'user'
+                },
+                order: [[ 'updateAt', 'DESC']]
+            }); 
+            const post = postData.map((post) => post.get ({ plain: true}))
+            res.render('homepage', {posts}); 
 
-//base homepage 
-router.get('/', (req, res) => { 
-    Post.findAll({ 
-        include:[User],
-    }).then((postData) =>{ 
-        console.log(postData); 
+        } catch (err) { 
+            res.status.apply(500).json(err); 
+        }
 
-      const posts = postData.map((post) => post.get({plain:true})); 
+    });
+       router.get('/login', (req, res) => {
+    try {
+        if (req.session.logged_in) {
+            res.redirect('/');
+            return;
+          }
+        
+          res.render('login');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
-      res.render('homepage', {  
-          post,
-      })
-    })
+router.get('/messages', async (req, res) => {
+    try {
+        const messageData = await Messages.findAll({
+            where: {user_id: req.session.user_id}, 
+            order:[['updatedAt',  'DESC']]
+        });
+
+        const userData = await User.findOne({ where: {id: req.session.user_id } });
+        console.log("userData", userData.name);
+
+        const messages = messageData.map((messages) => messages.get({ plain: true }));
+        messages.map(item => {
+            try {
+                item.messages = cryptr.decrypt(item.messages);   
+                item.username = userData.name;                
+                return item;
+            } catch(ex) {
+                console.error(ex);
+            }
+        });
+        
+        res.render('messages', { messages, logged_in: req.session.logged_in });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/pantry', async (req, res) => {
+
+    try {
+        const pantryData = await Product.findAll({
+            where: {user_id: req.session.user_id},
+            order:[['updatedAt',  'DESC']]
+        });
+        
+        const pantry = pantryData.map((pantry) => pantry.get({ plain: true }));
+        console.log(pantry)
+        res.render('pantry', { pantry, logged_in: req.session.logged_in });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/register', async (req, res) => {
+    try{
+        res.render('register')
+    } catch (err) {
+        res.status(500).json(err);
+    }
 })
+
+
+router.get('/create', async (req, res) => {
+    try{
+        res.render('create', {logged_in: req.session.logged_in})
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+router.get('/messageform', async (req, res) => {
+    try{
+        res.render('messageform', {logged_in: req.session.logged_in})
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+module.exports = router;
