25f334f1d6568e94abeb498ab218b98924d692f9
Anthony Ndegwa <anthon.n24@gmail.com>
2021-07-10 21:30:49 -0400

    server fix fro heroku6

--- server.js
+++ server.js
@@ -1,51 +1,46 @@
-///Dependencies
+//Dependencies 
 require('dotenv').config()
 const path = require('path');
-const express = require('express'); 
-const session = require('express-session'); 
-const routes = require('./controllers'); 
-const exphbs = require('express-handlebars'); 
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers')
 const hbs = exphbs.create({ helpers });
 
-
-const sequelize = require('./config/connection'); 
-const { Server } = require('http');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+//express app
+const app = express();
+const PORT = process.env.PORT || 3001;
 
- //express app
- const app = express(); 
- const port = process.env.PORT || 3000;
 
- const sess = { 
-   secret: process.env.SECRET,
-   cookies: {},
-   resave: false, 
-   saveUninitialized: false,
-   store: new SequelizeStore({
-     db: sequelize
-   })
- };
- 
-  app.use(session(sess));
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 
 //sets handlebars as default template 
-app.engine('handlebars', hbs.engine); 
-app.set('view engine', 'handlebars'); 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
-app.use(express.static(path.join(__dirname +'/public')));
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 //routes
-app.use(routes); 
+app.use(routes);
 
-{
-sequelize.sync({ force: false }).then(() => { 
-  app.listen(PORT, () => console.log('Listening on PORT'));
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
-
-
 
 
 
